d4f3bc342ebeda26ff10f0f43e9357a2e7ff506b
Sprees <alexrminnick@gmail.com>
2025-08-16 19:45:51 -0700

    completed

---
+++ script.js
@@ -0,0 +1,18 @@
+const socialsButton = document.querySelector('.article .btn.btn--icon');
+const socialsPopover = document.querySelector('.article__author-social-links[data-visible]');
+
+socialsButton.addEventListener('click', handleSocialsClick);
+
+function handleSocialsClick(e) {
+    // Turn the boolean string into a boolean value
+    const visible = JSON.parse(socialsPopover.getAttribute('data-visible'));
+
+    if(visible) {
+        socialsPopover.setAttribute('data-visible', false);
+        socialsButton.setAttribute('aria-expanded', false);
+    }
+    else {
+        socialsPopover.setAttribute('data-visible', true);
+        socialsButton.setAttribute('aria-expanded', true);
+    }
+}
